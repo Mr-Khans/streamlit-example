b6947bb368cc62327f21e87551f7acb3e581cc8c
Alexey Khanevski <alexey.hanevski@triplenext.com>
2022-08-25 23:47:34 +0300

    add cv2

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ import math
 import pandas as pd
 import streamlit as st
 import os
+import glob
+import cv2
 from PIL import Image
 import matplotlib.pyplot as plt
 
@@ -79,25 +81,12 @@ def hash_check_4(path_1,path_2,hash_size):
     return av, delta_5, delta_4
 
 def check_masks_l(path_one_mask, path_all_mask,hash_count):
-    size = (200, 300)
-    wb = Workbook()
-    ws = wb.active
-    ws["A1"] = "Img_1"
-    ws["B1"] = "Img_2"
-    ws["C1"] = "Filename"
-    ws["D1"] = "AVG_HASH" + str(hash_count)+ 'x' + str(hash_count) + " L"
-    ws["E1"] = "d_hash_v"
-    ws["F1"] = "d_hash"
-    ws["G1"] = "p_hash"
-    ws["H1"] = "a_hash"
     for folder_all_mask in glob.glob(path_all_mask):
         
-        score , delta_5, delta_4 = hash_check(path_one_mask, folder_all_mask,hash_count)
+        score , delta_5, delta_4 = hash_check_4(path_one_mask, folder_all_mask,hash_count)
         list_score.append(score)
         list_mask.append(folder_all_mask)
-        insert_row(ws,  path_one_mask, folder_all_mask, os.path.basename(folder_all_mask),score,delta_5, delta_4, delta_3, delta_2, size=size)
-    wb.save('/content/result/new_test_L' + str(hash_count)+ 'x' + str(hash_count) +'.xlsx')
-    wb.close()
+
 
 def load_image(image_file):
 	img = Image.open(image_file)
