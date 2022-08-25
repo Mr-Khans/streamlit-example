850dd3593c32bdc5c3aeda36869c25407b9b9ba2
Alexey Khanevski <alexey.hanevski@triplenext.com>
2022-08-25 19:43:02 +0300

    clear code

--- streamlit_app.py
+++ streamlit_app.py
@@ -142,14 +142,25 @@ def load_image(image_file):
 
 
 if __name__ == '__main__':
-    menu = ['Image Detection', 'Video Detection']
-	choice = st.sidebar.selectbox('Menu',menu)
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+         # To read file as bytes:
+         bytes_data = uploaded_file.getvalue()
+         st.write(bytes_data)
 
-	if choice == 'Image Detection':
-		st.subheader('**Face Mask Detection**')
-		image_file = st.file_uploader("Upload an image", type=['png', 'jpg', 'jpeg'])
-        		
-		if image_file is not None:
-			our_image = Image.open(image_file)
-			st.text("Original Image")
-			st.image(our_image)
+         # To convert to a string based IO:
+         stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+         st.write(stringio)
+
+         # To read file as string:
+         string_data = stringio.read()
+         st.write(string_data)
+
+         # Can be used wherever a "file-like" object is accepted:
+         dataframe = pd.read_csv(uploaded_file)
+         st.write(dataframe)
+    uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+    for uploaded_file in uploaded_files:
+         bytes_data = uploaded_file.read()
+         st.write("filename:", uploaded_file.name)
+         st.write(bytes_data)
