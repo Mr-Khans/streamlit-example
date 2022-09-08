79f0ed314b3f0dc891822960a15c34c66d8bd2ac
Alexey <vraizen@mail.ru>
2022-09-09 02:17:30 +0300

    fix minor

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ def image_diff(pic_1, pic_2):
 st.header("Tast same mask")
 # Uploading the File to the Page
 uploadFile = st.file_uploader(label="Upload mask for test", type=['jpg', 'png', 'jpeg'])
-uploadFile_ = st.file_uploader(label="Upload mask in dataset", accept_multiple_files=True, type=['jpg', 'png', 'jpeg'])
+uploadFile_ = st.file_uploader(label="Upload mask in dataset", accept_multiple_files=False, type=['jpg', 'png', 'jpeg'])
 #uploadFile_ = st.file_uploader(label="Upload mask in dataset",  type=['jpg', 'png', 'jpeg'])
 
 # Checking the Format of the page
