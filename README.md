0295a25b821f06a373e9d78cfa4c52d793111543
Alexey Khanevski <alexey.hanevski@triplenext.com>
2023-03-03 15:31:59 +0300

    update code and fix minor

--- README.md
+++ README.md
@@ -1,6 +1,42 @@
 # Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
+# Image Similarity Detection
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+This is a project that uses computer vision to determine the similarity between images. Specifically, we use machine learning algorithms to automatically find similarities and differences between two images, and then display these results on a web page using the Streamlit library.
+
+### Installation Dependencies
+To run this project, you need to install the following dependencies:
+```sh
+Python 3.6+
+streamlit==1.4.0
+numpy
+pillow
+scikit-image
+altair
+scipy
+tensorflow_hub
+opencv-contrib-python-headless
+tensorflow==2.7.0
+pandas
+```
+
+You can install them by running the following command:
+```sh
+pip install -r requirements.txt
+```
+
+### How to Use
+You can run the project by executing the command:
+```sh
+streamlit run app.py
+```
+
+After running, you can upload two images by clicking on the corresponding button, and then click on the "Compare" button to see the results. You will see the similarity measure between the two images based on the Pearson correlation coefficient, which will be displayed on the page as a number from 0 to 1.
+
+### Example Images
+
+Example images for testing are located in the test_img folder, including the files a (3).png and a (1).png.
+
+### Authors
+
+This project was created by Alexey Kh.
--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,6 @@ import os
 import glob
 import cv2
 
-
 #init paremeters
 metric = 'cosine'
 #accuracy - [+/- 0.01]
@@ -25,71 +24,85 @@ IMAGE_SHAPE = (224, 224)
 layer = hub.KerasLayer(model_url)
 model = tf.keras.Sequential([layer])
 
-# Function to Read and Manupilate Images
-def load_image(img):
+# Function to Read and Manipulate Images
+def load_image(img: str) -> np.ndarray:
+    """
+    Reads and resizes the input image
+    
+    Args:
+    img: The image file name
+    
+    Returns:
+    The resized image as a numpy array
+    """
     im = Image.open(img).convert('L').resize(IMAGE_SHAPE)
     image = np.array(im)
     return image
 
 
-#def embedding for image
-def extract(file):
-  file = Image.open(file).convert('L').resize(IMAGE_SHAPE)
-  #display(file)
-  file = np.stack((file,)*3, axis=-1)
-  file = np.array(file)/255.0
-  embedding = model.predict(file[np.newaxis, ...])
-  #print(embedding)
-  vgg16_feature_np = np.array(embedding)
-  flattended_feature = vgg16_feature_np.flatten()
-  #print(len(flattended_feature))
-  #print(flattended_feature)
-  #print('-----------')
-  return flattended_feature
+# Function to Extract Embeddings for Image
+def extract(file: str) -> np.ndarray:
+    """
+    Extracts embeddings for the given image file
+    
+    Args:
+    file: The image file name
+    
+    Returns:
+    The flattened feature vector as a numpy array
+    """
+    file = Image.open(file).convert('L').resize(IMAGE_SHAPE)
+    file = np.stack((file,)*3, axis=-1)
+    file = np.array(file)/255.0
+    embedding = model.predict(file[np.newaxis, ...])
+    vgg16_feature_np = np.array(embedding)
+    flattended_feature = vgg16_feature_np.flatten()
+    return flattended_feature
 
 
-def image_diff(pic_1, pic_2):
+def image_diff(pic_1: str, pic_2: str) -> float:
+    """
+    Calculates the difference between two images
+    
+    Args:
+    pic_1: The first image file name
+    pic_2: The second image file name
+    
+    Returns:
+    The distance between the two images
+    """
     img_1 = extract(pic_1)
-    #org_img = cv2.imread(path_mask)
-    #for folder_all_mask in glob.glob(mask_path_xl):
     img_2 = extract(pic_2)
-    #second_img = cv2.imread(folder_all_mask)
     dc = distance.cdist([img_1], [img_2], metric)[0]
-      #list_score.append(float(dc))
-      #list_mask.append(folder_all_mask)
     return dc
-    #   plt.subplot(121)
-    #   plt.imshow(org_img)
-    #   plt.subplot(122)
-    #   plt.imshow(second_img)
-    #   plt.show()
-    #   print(folder_all_mask, float(dc))
+
+
+
+# Streamlit App
 st.header("TEST mask")
 # Uploading the File to the Page
 uploadFile = st.file_uploader(label="Upload mask for test", type=['jpg', 'png', 'jpeg'])
 uploadFile_ = st.file_uploader(label="Upload mask in dataset", accept_multiple_files=False, type=['jpg', 'png', 'jpeg'])
-#uploadFile_ = st.file_uploader(label="Upload mask in dataset",  type=['jpg', 'png', 'jpeg'])
+
 
 # Checking the Format of the page
 if uploadFile is not None:
-    
-    # Perform your Manupilations (In my Case applying Filters)
+    # Perform your Manipulations (In my Case applying Filters)
     img_1 = load_image(uploadFile)
     st.image(img_1)
     st.write("Image Uploaded Successfully")
 else:
-    st.write("Make sure you image is in JPG/PNG/JPEG Format.")
+    st.write("Make sure your image is in JPG/PNG/JPEG Format.")
 
 
 # Checking the Format of the page
 if uploadFile_ is not None:
-    #for i in range()
-    # Perform your Manupilations (In my Case applying Filters)
+    # Perform your Manipulations (In my Case applying Filters)
     img_2 = load_image(uploadFile_)
     st.image(img_2)
     st.write("DATASET Uploaded Successfully")
 else:
-    st.write("Make sure you image is in JPG/PNG/JPEG Format.")
+    st.write("Make sure your image is in JPG/PNG/JPEG Format.")
 
 
 if st.button('Result'):
